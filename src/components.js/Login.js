e757b0a206713f1823aab95817aa4f39fc8de681
MeghashreeK <kundermeghashree279@gmail.com>
2024-05-28 15:15:27 +0530

    login styling done

--- src/components.js/Login.js
+++ src/components.js/Login.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import Header from './Header'
+import React, { useState } from 'react';
+import Header from './Header';
 
 const Login = () => {
-  return (
-    <div className='relative'>
-      <Header/>
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="netflix-bg"/>
-    
-    </div>
-  )
-}
+  const [isSignIn, setIsSignIn] = useState([]);
 
-export default Login
+  return (
+    <div className='relative w-full h-screen'>
+      <Header />
+      <img
+        className='absolute top-0 left-0 w-full h-full object-cover -z-10'
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        alt="netflix-bg"
+      />
+      <form className='flex flex-col justify-center items-center absolute bg-black bg-opacity-80 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 max-w-md p-8 rounded'>
+        <div className='flex flex-col w-full gap-5'>
+          <p className='text-white font-bold text-3xl'>Sign In</p>
+          <div className='flex flex-col gap-2'>
+            <input
+              className='p-3 rounded-sm bg-gray-800 bg-opacity-40 text-white w-full'
+              type='text'
+              placeholder='Email'
+            />
+            <input
+              className='p-3 rounded-sm bg-gray-800 bg-opacity-40 text-white w-full'
+              type='password'
+              placeholder='Password'
+            />
+            <button className='bg-red-600 p-3 w-full text-white rounded hover:bg-red-500'>Sign In</button>
+          </div>
+          <div className='flex gap-1 justify-center'>
+            <p className='text-white'>Not registered yet?</p>
+            <p className='text-white hover:text-red-600 cursor-pointer'>Sign Up now!</p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

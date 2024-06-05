2c2457081c4eaf149c5694f974578d43e0da55b8
MeghashreeK <kundermeghashree279@gmail.com>
2024-06-05 14:19:21 +0530

    signout

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import Header from './Header'
+import { useDispatch, useSelector } from 'react-redux';
+import { signOut } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { removeUser } from '../utils/UserSlice';
+import { useNavigate } from 'react-router-dom';
+
 
 const Browser = () => {
+  const imgData = useSelector((store) => store.userdetails.user);
+  const picURL = imgData[0].photoURL;
+  const dispatch = useDispatch();
+  const navigate=useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      dispatch(removeUser());
+      navigate("/");
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode + "-" + errorMessage);
+    });
+  }
+
   return (
     <div>
       <div className='flex justify-between'>
-        <div>
-      <Header/>
-      </div>
-      <div>
-      <button className='font-bold text-red-500'>Sign Out</button>
-      </div>
+        <div className='absolute left-0 top-0 -z-10 w-full'>
+          <Header />
+        </div>
+        <div className='flex p-2 gap-4 absolute top-0 sm:left-[93%] left-[75%]'>
+          <img className='w-8 h-8' alt="profile-pic" src={picURL} />
+          <img className='w-8 h-8 cursor-pointer' onClick={() => handleSignOut()} src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/c53030/external-logout-screen-from-the-working-portfolio-of-a-user-classic-bold-tal-revivo.png" alt="external-logout-screen-from-the-working-portfolio-of-a-user-classic-bold-tal-revivo" />
+        </div>
       </div>
     </div>
+
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,9 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='absolute z-10 w-full'>
-            {/* <img className='w-48 bg-gradient-to-b from-black' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" /> */}
-            <h1 className='font-bold p-2 text-red-600 Bebas text-4xl bg-gradient-to-b from-black relative'>
-            <span className='block'>
-                <span className='transform -translate-y-2'>N</span>
-                <span className='transform -translate-y-1'>I</span>
-                <span className=''>R</span>
-                <span className='transform translate-y-1'>V</span>
-                <span className='transform translate-y-2'>A</span>
-                <span className='transform -translate-y-1'>N</span>
-                <span className='transform -translate-y-2'>A</span>
-            </span>
-        </h1>        </div>
+            <h1 className='font-bold p-2 text-red-600 Bebas text-4xl bg-gradient-to-b from-black relative'>NIRVANA</h1>
+        </div>
     )
 }
 
-export default Header
+export default Header;

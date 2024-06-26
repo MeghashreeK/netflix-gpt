056910acc9449a06e96dfc837e4a3a99c5f4bd0b
MeghashreeK <kundermeghashree279@gmail.com>
2024-06-26 17:39:20 +0530

    made GptSearch responsive

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -6,9 +6,9 @@ const GptSearch = () => {
   const language=useSelector(store=>store.language.languageSelected);
   console.log(language);
   return (
-       <div className='flex justify-center bg-[#010B13] pt-[8%] h-screen w-screen gap-2'>
+       <div className='flex justify-center bg-[#010B13] pt-[20%] sm:pt-[8%] h-screen w-screen gap-2'>
         <input className="h-5 w-1/2 p-5 rounded-lg focus:outline-none" placeholder={lang[language].gptSearchPlaceholder}/>
-        <button className='flex text-white h-5 bg-red-600 p-5 items-center rounded-lg transition duration-1000 hover:opacity-70'>{lang[language].search}</button>
+        <button className='flex text-white h-5 bg-red-600 p-5 items-center rounded-lg transition duration-500 hover:opacity-70'>{lang[language].search}</button>
        </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -48,20 +48,23 @@ const Header = () => {
 
     return (
         <div className='absolute z-10'>
-            <div className='flex pt-3 px-10 text-red-600 bg-gradient-to-b from-black w-screen relative justify-between'>
+            <div className='flex pt-3 px-4 sm:px-10 text-red-600 bg-gradient-to-b from-black w-screen relative justify-between'>
                 <p className='font-bold Bebas text-4xl'>NIRVANA</p>
                 {window.location.pathname === "/browse" && (
                     <div className='flex flex-col gap-1'>
                         <div className='flex items-center gap-1'>
-                            {(gptSearchValue) && <select className='border-2' onChange={handleLanguageSelection}>
+                            {/* {(gptSearchValue) && <select className='border-2' onChange={handleLanguageSelection}>
                                 {LANGUAGE_SUPPORT.map((lang) => <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
-                            </select>}
+                            </select>} */}
                             <img width="24" height="24" src={arrowState ? "https://img.icons8.com/material-sharp/24/ffffff/sort-up.png" : "https://img.icons8.com/material-sharp/24/ffffff/sort-down.png"} alt="sort-down" onClick={handleArrowIconFunction} />
                             <img className='w-8 h-8 cursor-pointer' onClick={handleSignOut} src="https://occ-0-1492-3662.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e" alt="profile-icon" />
                         </div>
-                        {(arrowState) && <div className='border-2 text-white p-2'>
+                        {(arrowState) && <div className='border-2 text-white p-2 bg-gray-400'>
                             <p className='cursor-pointer' onClick={() => dispatch(gptToggleFunction())}>{gptSearchValue ? "Home" : "Gpt Search"}</p>
                             <p className='cursor-pointer'>Sign Out</p>
+                            {(gptSearchValue) && <select className='bg-gray-400' onChange={handleLanguageSelection}>
+                                {LANGUAGE_SUPPORT.map((lang) => <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
+                            </select>}
                         </div>}
                     </div>
                 )
